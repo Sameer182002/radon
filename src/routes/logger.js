19ff0f4a7a76baf3ad23f84a8cd5eccb28b8a2ab
Sameer182002 <1978sameerbajaj@gmail.com>
2022-06-01 15:53:51 +0530

    Nodejs Modules By sameer

---
+++ src/routes/logger.js
@@ -0,0 +1,9 @@
+const endpoint="https://www.functionup.org"
+const batch ="Radon"
+const log=function (){
+
+    console.log("Hi!! This is a message ")
+}
+module.exports.url=endpoint;
+module.exports.batch=batch
+module.exports.log=log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
+const logger=require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
-
+router.get('/test-me1', function (req, res) {
+    console.log("The constant in logger point has a value " + logger.url)
+    console.log("The current batch is  " + logger.batch)
+    console.log("The current function is  " + logger.log)
+    res.send('My second ever api!')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('My three ever api!')
+});
+router.get('/test-me3', function (req, res) {
+    res.send('My last ever api!')
+});
 module.exports = router;
 // adding this comment for no reason
